1af27103d762d3a53753a18107dea458ccfdf1df
manoj <manoj@alagesan.fr>
2022-03-16 11:06:08 +0100

    test2

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ Created on Wed Mar 16 10:09:31 2022
 print("hello world manoj")
 print("test")
 print("it work")
-<<<<<<< HEAD
 print("j'essaie")
-=======
 print("Testwo")
->>>>>>> 1537f6c9bb62c7b963e584d54db30eeaa1f871a9
 print("c'est carré")
+
+
+for i in range (3):
+    print("a")
